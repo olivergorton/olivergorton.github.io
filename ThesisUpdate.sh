aa4a495ec0de58b4479c08c588e45e85c0e5ab55
olivergorton <olivergorton@gmail.com>
2019-07-17 08:50:43 -0700

    Delete ThesisUpdate.sh

--- ThesisUpdate.sh
+++
@@ -1,6 +0,0 @@
-cp ~/Dropbox/Gorton_MSPhysics_Thesis/thesis.pdf ./
-cp ~/Dropbox/Gorton_MSPhysics_Thesis/Presentation/masters-thesis.pdf ./
-
-git add ./*
-git commit -m "script"
-git push origin master
